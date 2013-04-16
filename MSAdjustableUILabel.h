6dfc53738ec3f8bb3181aaf58daf8854fcbf50d7
Serban Mihai <mihaiserban2@yahoo.com>
2013-04-16 15:51:01 +0300

    Create MSAdjustableUILabel.h

---
+++ MSAdjustableUILabel.h
@@ -0,0 +1,18 @@
+//
+//  MSAdjustableUILabel.h
+//  Adjustable character spacing UILabel for iOS
+//
+//  Created by Mihai Serban on 4/16/13.
+//  Copyright (c) 2013 http://flavors.me/mihaiserban. All rights reserved.
+//  Distribuited under MIT License
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSAdjustableUILabel : UILabel {
+    CGFloat characterSpacing;
+}
+
+@property CGFloat characterSpacing;
+
+@end
